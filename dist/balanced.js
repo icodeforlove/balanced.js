04bf8014d8129a02a9725e21d85f1d2bccac83f3
Chad Scira <chadvscira@gmail.com>
2014-08-02 20:07:13 +0700

    rebuilt diet for v0.0.5

--- dist/balanced.js
+++ dist/balanced.js
@@ -1,5 +1,5 @@
 /**
- * balanced.js v0.0.4
+ * balanced.js v0.0.5
  */
 var balanced =
 /******/ (function(modules) { // webpackBootstrap
@@ -50,92 +50,156 @@ var balanced =
 
 	// Note: this currently doesn't support nested replacements because its meant to be 
 	// greedy and grab the first head all the way to the last
-	function escapeRegExp(str) {
-	  return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+	function Balanced (config) {
+		config = config || {};
+		
+		if (!config.right) throw new Error('Balanced: please provide a "right" property');
+		if (!config.left) throw new Error('Balanced: please provide a "left" property');
+	
+		this.head = config.head || config.right;
+		this.balance = config.balance || false;
+		this.exceptions = config.exceptions || false;
+		this.left = config.left;
+		this.right = config.right;
 	}
 	
-	function matchContentsInBetweenBrackets(string, head, right, left) {
-		var caseInsensitive = head instanceof RegExp && head.ignoreCase,
-			headRegExp = head instanceof RegExp ? head : new RegExp(escapeRegExp(head)),
-			rightRegExp = right instanceof RegExp ? right : new RegExp(escapeRegExp(right)),
-			leftRegExp = left instanceof RegExp ? left : new RegExp(escapeRegExp(left)),
-			regex = new RegExp(
-				headRegExp.source + '|' + 
-				rightRegExp.source + '|' + 
-				leftRegExp.source,
-				'g' + (caseInsensitive ? 'i' : '')
-			),
-			matches = [],
-			matchedOpening = null,
-			depth,
-			match;
-	 
-		while ((match = regex.exec(string))) {
-			if (!matchedOpening && match[0].match(headRegExp)) {
-				matchedOpening = match;
-				depth = 1;
-			} else if (match[0].match(rightRegExp)) {
-				depth++;
-			} else if (match[0].match(leftRegExp)) {
-				depth--;
+	Balanced.prototype = {
+		/**
+		 * Escapes a string to be used within a RegExp
+		 * @param  {String} string
+		 * @return {String}
+		 */
+		escapeRegExp: function (string) {
+		  return string.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+		},
+	
+		/**
+		 * Matches contents
+		 * 
+		 * @param  {String} string
+		 * @return {String}
+		 */
+		matchContentsInBetweenBrackets: function (string) {
+			var caseInsensitive = this.head instanceof RegExp && this.head.ignoreCase,
+				headRegExp = this.head instanceof RegExp ? this.head : new RegExp(this.escapeRegExp(this.head)),
+				rightRegExp = this.right instanceof RegExp ? this.right : new RegExp(this.escapeRegExp(this.right)),
+				leftRegExp = this.left instanceof RegExp ? this.left : new RegExp(this.escapeRegExp(this.left)),
+				regex = new RegExp(
+					headRegExp.source + '|' + 
+					rightRegExp.source + '|' + 
+					leftRegExp.source,
+					'g' + (caseInsensitive ? 'i' : '')
+				),
+				matches = [],
+				matchedOpening = null,
+				depth = 0,
+				match,
+				balanced = true;
+	
+			while ((match = regex.exec(string))) {
+				if (!matchedOpening && match[0].match(headRegExp) && (!this.balance || this.balance && !depth)) {
+					matchedOpening = match;
+					depth = this.balance ? depth + 1 : 1;
+				} else if (match[0].match(rightRegExp)) {
+					depth++;
+				} else if (match[0].match(leftRegExp)) {
+					depth--;
+				}
+	
+				if (this.balance && depth < 0) {
+		            balanced = false;
+		            if (this.exceptions) {
+		            	throw new Error ('Balanced: expected right bracket at ' + match.index);
+		            }
+		            break;
+		        }
+		 
+				if (matchedOpening !== null && depth === 0) {
+					matches.push({
+						index: matchedOpening.index, 
+						length: match.index - matchedOpening.index,
+						head: matchedOpening[0],
+						tail: match[0]
+					});
+					matchedOpening = null;
+				}
 			}
-	 
-			if (matchedOpening !== null && depth === 0) {
-				matches.push({
-					index: matchedOpening.index, 
-					length: match.index - matchedOpening.index,
-					head: matchedOpening[0],
-					tail: match[0]
-				});
-				matchedOpening = null;
+			if (this.balance) {
+				if (this.exceptions && !(balanced && depth === 0)) {
+					throw new Error ('Balanced: expected left bracket at ' + (string.length -1));
+				}
+				return balanced && depth === 0 ? matches : null;
+			} else {
+				return matches;
 			}
-		}
-	 
-		return matches;
-	}
-	 
-	function replaceMatchesInString(matches, string, replace) {
-		var offset = 0;
-		
-		for (var i = 0; i < matches.length; i++) {
-			var match = matches[i],
-				replacement = replace(string.substr(match.index + offset + match.head.length, match.length - match.head.length), match.head, match.tail);
-			string = string.substr(0, match.index + offset) + replacement + string.substr(match.index + offset + match.length + match.tail.length, (string.length) - (match.index + offset + match.length));
+		},
+	
+		/**
+		 * Non-destructive match replacements.
+		 * 
+		 * @param  {Array} matches
+		 * @param  {String} string
+		 * @param  {Function} replace
+		 * @return {String}
+		 */
+		replaceMatchesInString: function (matches, string, replace) {
+			var offset = 0;
 			
-			offset += replacement.length - match.length - match.tail.length;
-		}
-		
-		return string;
-	}
-	 
-	function replaceMatchesInBetweenBrackets(string, start, right, left, replace) {
-		var matches = matchContentsInBetweenBrackets(string, start, right, left);
-		return replaceMatchesInString(matches, string, replace);
-	}
+			for (var i = 0; i < matches.length; i++) {
+				var match = matches[i],
+					replacement = replace(string.substr(match.index + offset + match.head.length, match.length - match.head.length), match.head, match.tail);
+				string = string.substr(0, match.index + offset) + replacement + string.substr(match.index + offset + match.length + match.tail.length, (string.length) - (match.index + offset + match.length));
+				
+				offset += replacement.length - match.length - match.tail.length;
+			}
+			
+			return string;
+		},
 	
-	exports.replaceMatchesInString = replaceMatchesInString; 
+		/**
+		 * Runs replace function against matches, and source.
+		 * 
+		 * @param  {String} string
+		 * @param  {Function} replace
+		 * @return {String}
+		 */
+		replaceMatchesInBetweenBrackets: function (string, replace) {
+			var matches = this.matchContentsInBetweenBrackets(string);
+			return this.replaceMatchesInString(matches, string, replace);
+		}
+	};
+	
+	exports.replaceMatchesInString = Balanced.prototype.replaceMatchesInString; 
 	
 	exports.replacements = function (config) {
 		config = config || {};
-		config.head = config.head || config.right;
+	
+		var balanced = new Balanced({
+			head: config.head,
+			right: config.right,
+			left: config.left,
+			balance: config.balance,
+			exceptions: config.exceptions
+		});
 	
 		if (!config.source) throw new Error('Balanced: please provide a "source" property');
-		if (!config.right) throw new Error('Balanced: please provide a "right" property');
-		if (!config.left) throw new Error('Balanced: please provide a "left" property');
 		if (typeof config.replace !==  'function') throw new Error('Balanced: please provide a "replace" function');
 	
-		return replaceMatchesInBetweenBrackets(config.source, config.head, config.right, config.left, config.replace);
+		return balanced.replaceMatchesInBetweenBrackets(config.source, config.replace);
 	};
 	
 	exports.matches = function (config) {
-		config = config || {};
-		config.head = config.head || config.right;
+		var balanced = new Balanced({
+			head: config.head,
+			right: config.right,
+			left: config.left,
+			balance: config.balance,
+			exceptions: config.exceptions
+		});
 	
 		if (!config.source) throw new Error('Balanced: please provide a "source" property');
-		if (!config.right) throw new Error('Balanced: please provide a "right" property');
-		if (!config.left) throw new Error('Balanced: please provide a "left" property');
-		
-		return matchContentsInBetweenBrackets(config.source, config.head, config.right, config.left);
+	
+		return balanced.matchContentsInBetweenBrackets(config.source);
 	};
 
 /***/ }
